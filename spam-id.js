fe9379c46328dc14dd9b863815fd117222e30f1a
j3dev <cipherphage@users.noreply.github.com>
2020-04-30 21:03:18 -0700

    corrected logic around words object in word choice and frequency calc, corrected if else flow in same function, tests pass

--- spam-id.js
+++ spam-id.js
@@ -1,9 +1,13 @@
+// global limits
+var iLim = 0.59; // individual limit (matches specific spam example >= 60%)
+var tLim = 3.2;
+
 // helper functions
 var calcNumWords = (sf, ef) => {
   if (sf.num_words == ef.num_words) {
     return 0.2;
-  } else if (Math.abs(sf.num_words - ef.num_words) < 15) {
-    return 0.1;
+  } else if (Math.abs(sf.num_words - ef.num_words) < 100) {
+    return 0.2;
   } else {
     return 0.0
   }
@@ -12,8 +16,8 @@ var calcNumWords = (sf, ef) => {
 var calcNumUnique = (sf, ef) => {
   if (sf.num_unique_words == ef.num_unique_words) {
     return 0.2;
-  } else if (Math.abs(sf.num_unique_words - ef.num_unique_words) < 3) {
-    return 0.1;
+  } else if (Math.abs(sf.num_unique_words - ef.num_unique_words) < 50) {
+    return 0.2;
   } else {
     return 0.0;
   }
@@ -26,51 +30,47 @@ var calcWordCF = (sf, ef) => {
   let words = Object.keys(sf.words_object);
 
   words.forEach(w => {
-    if (ef.hasOwnProperty(w)) {
+    if (ef.words_object.hasOwnProperty(w)) {
       uCount++;
-      if ((ef[w] > 3) && (Math.abs(ef[w] - sf[w]) < 1)) {
+      if ((ef[w] > 2) && (Math.abs(ef[w] - sf[w]) < 1)) {
         fCount++;
       }
     }
   });
-
+  
   let uniqueRatio = uCount/sf.num_unique_words;
-  let freqRatio = fCount/uCount;
-
-  if (uniqueRatio > 0.95) {
-    score = score + 0.2;
-  } else if (uniqueRatio > 0.9) {
-    score = score + 0.15;
-  } else if (uniqueRatio > 0.85) {
+  let freqRatio = uCount > 0 ? fCount/uCount : 0.0;
+  
+  if (uniqueRatio < 0.6) {
+    score = score + 0.0;
+  } else if (uniqueRatio < 0.7) {
     score = score + 0.10;
   } else if (uniqueRatio > 0.8) {
-    score = score + 0.05;
-  } else {
-    score = score + 0.0;
-  }
-
-  if (freqRatio > 0.95) {
-    score = score + 0.2;
-  } else if (freqRatio > 0.9) {
     score = score + 0.15;
-  } else if (freqRatio > 0.85) {
+  } else if (uniqueRatio > 0.90) {
+    score = score + 0.2;
+  } 
+  
+  if (freqRatio < 0.6) {
+    score = score + 0.0;
+  } else if (freqRatio < 0.7) {
     score = score + 0.10;
   } else if (freqRatio > 0.8) {
-    score = score + 0.05;
-  } else {
-    score = score + 0.0;
-  }
-
+    score = score + 0.15;
+  } else if (freqRatio > 0.9) {
+    score = score + 0.2;
+  } 
+  
   return score;
 };
 
 var calcLength = (sf, ef) => {
   if (sf.total_length == ef.total_length) {
     return 0.2;
-  } else if (Math.abs(sf.total_length - ef.total_length) < 25) {
-    return 0.15;
-  } else if (Math.abs(sf.total_length - ef.total_length) < 50) {
-    return 0.10;
+  } else if (Math.abs(sf.total_length - ef.total_length) < 250) {
+    return 0.2;
+  } else if (Math.abs(sf.total_length - ef.total_length) < 500) {
+    return 0.2;
   } else {
     return 0.0
   }
@@ -83,26 +83,27 @@ var spamIdentifier = (spams, emails) => {
   let emailCollection = new Email(emails);
   let spamf = spamCollection.get_factors;
   let emailf = emailCollection.get_factors;
-  console.log("spam check");
-  console.log(spamCollection);
 
   let results = emailf.map(ef => {
     let r = 0.0;
     spamf.forEach(sf => {
-      r = calcNumWords(sf.factors, ef.factors) + calcNumUnique(sf.factors, ef.factors) +
+      let rr = calcNumWords(sf.factors, ef.factors) + calcNumUnique(sf.factors, ef.factors) +
         calcWordCF(sf.factors, ef.factors) + calcLength(sf.factors, ef.factors);
-      return r;
+      if (rr > iLim) {
+        r = tLim + 1;
+        return r;
+      }
+      return r + rr;
     });
     return r;
   });
   
   emailCollection.emails.forEach((e,i) => {
     e.spamCheck = true;
-    if (results[i] > 3.2) {
+    if (results[i] > tLim) {
       e.isSpam = true;
     }
   });
-  console.log("email check");
-  console.log(emailCollection);
+  
   return emailCollection;
 };
